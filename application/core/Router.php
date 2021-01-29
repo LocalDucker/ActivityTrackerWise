806fe941cb49a5cf3ea91c3d594423843f4913c4
LocalDucker <alkaiiah909@gmail.comgit config --global user.email alkaiiah909@gmail.com>
2021-01-29 04:01:36 +0200

    Started to create MVC

---
+++ application/core/Router.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace application\core;
+
+class Router {
+
+    protected $routes = [];
+    protected $params = [];
+
+    function __construct(){
+        //
+    }
+
+    function add() {
+        //
+    }
+
+    function match(){
+        //
+        
+    }
+
+    function run(){
+        echo 'Start';
+    }
+
+}
