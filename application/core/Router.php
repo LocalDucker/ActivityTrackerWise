b81089dae7273bb07af459d2d5434d7df8ae470f
LocalDucker <alkaiiah909@gmail.comgit config --global user.email alkaiiah909@gmail.com>
2021-01-29 04:41:04 +0200

    Added routes and functions in Router.php

--- application/core/Router.php
+++ application/core/Router.php
@@ -8,20 +8,47 @@ class Router {
     protected $params = [];
 
     function __construct(){
-        //
+        $routes = require 'application/routes/routes.php';
+        $this->add($routes);
     }
 
     function add() {
-        //
+        foreach ($routes as $route => $params) {
+            $route = '#^\\' . $route . '$#';
+            $this->routes[$route] = $params;
+        }
     }
 
     function match(){
-        //
-        
+        $url = trim($_SERVER['REQUEST_URI']);
+        foreach ($this->routes as $route => $params) {
+            if (preg_match($route, $url)) {
+                $this->params = $params;
+                return true;
+            }
+        }
+        return false;    
     }
 
     function run(){
-        echo 'Start';
+        if ($this->match()) {
+            $path = 'app\controllers\\' . $this->params['controller'];
+            if (class_exists($path)) {
+                $action = $this->params['action'];
+                if (method_exists($path, $action)) {
+                    $controller = new $path($this->params);
+                    $controller->$action();
+                } else {
+                    View::error(404);
+                }
+
+            } else {
+                View::error(404);
+            }
+
+        } else {
+            View::error(404);
+        }
     }
 
 }
---
+++ application/routes/routes.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    // PageController
+    '/' => [
+        'controller' => 'PageController',
+        'action' => 'index'
+    ]
+
+    
+]
