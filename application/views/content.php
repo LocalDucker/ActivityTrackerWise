ff02565c73b34b964051e37a300d69c124668fd2
LocalDucker <alkaiiah909@gmail.comgit config --global user.email alkaiiah909@gmail.com>
2021-01-30 20:52:59 +0200

    cascade front

---
+++ application/views/content.php
@@ -0,0 +1,36 @@
+<div class="wrapper">
+    <div class="header">
+        <div class="top-block">
+            
+        </div>
+
+        <div class="info-block">
+
+            <form action="">
+                <input type="datetime" name="start_time">
+                <input type="datetime" name="finish_time">
+                <input type="number" min="0" name="distance">
+                <select name="activity" id="">
+                    <option disabled selected>select</option>
+                </select>
+                <button name="btn">Save</button>
+            </form>
+        </div>
+    </div>
+
+
+    <div class="content">
+        <div class="info-left">
+        
+        </div>
+
+        <div class="info-right">
+            <div class="top"></div>
+            <div class="bottom"></div>
+        </div>
+
+    </div>
+
+
+ </div>
+
