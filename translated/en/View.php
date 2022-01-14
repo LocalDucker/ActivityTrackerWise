d1983c07cb96d76df531909bdbefde287a0e0c22
LocalDucker <42616614+LocalDucker@users.noreply.github.com>
2022-01-14 18:30:36 +0200

    New translations View.php (English)

---
+++ translated/en/View.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace application\core;
+
+class View{
+    
+    public $path;
+    public $route; 
+
+    public function __construct($route){
+        $this->route = $route;
+        $this->path = $route['controller'] . '/' . $route['action'];
+        
+    }
+
+    public function render($page, $vars = []){
+        extract($vars);
+        $path = 'application/views/'. $page .'.php';
+        if (file_exists($path)) {
+			ob_start();
+			require $path;
+			$content = ob_get_clean();
+			require 'application/views/layout.php';
+		}
+    }
+    	static public function redirect($url) {
+		header('location: '.$url);
+		exit;
+	}
+
+    public static function errorCode($code) {
+		http_response_code($code);
+		$path = 'application/views/'.$code.'.php';
+		if (file_exists($path)) {
+			require $path;
+		}
+		exit;
+    }
+    
+}
